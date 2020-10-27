7d8107f45c31cfdf03eabec8bfb523a4894d337c
Ignacio <ignacionrmesa19>
2020-10-27 10:27:35 +0100

    Añadimos comprobación formato matrícula

--- src/utility/lecturaTeclado.ts
+++ src/utility/lecturaTeclado.ts
@@ -24,9 +24,10 @@ export let leeMatricula = async (prompt: string) => {
     let matricula: string
     matricula = await leeLinea(prompt)
     readlineI.close()
+    let letras:RegExp=/B|C|D|F|G|H|J|K|L|M|N|P|Q|R|S|T|V|W|X|Y|Z/
     if(matricula.length!=7 || !matricula.charAt(0).match(/[0-9]/) || !matricula.charAt(1).match(/[0-9]/) || !matricula.charAt(2).match(/[0-9]/)
-    || !matricula.charAt(3).match(/[0-9]/) || !matricula.charAt(4).match(/[A-Z]/)|| !matricula.charAt(5).match(/[A-Z]/)|| !matricula.charAt(6).match(/[A-Z]/)){
-        throw 'ERROR la matrícula no tiene el formato correcto NNNNXXX'
+    || !matricula.charAt(3).match(/[0-9]/) || !matricula.charAt(4).match(letras) || !matricula.charAt(5).match(letras)|| !matricula.charAt(6).match(letras)){
+        throw 'ERROR la matrícula no tiene el formato correcto NNNNXXX sin vocales'
     } else {
         return matricula
     }
